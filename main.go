bc4615ba95466564f202673467e87e3b3e80ee21
kaigoman <kaigo@protonmail.com>
2020-10-17 11:59:03 +0100

    add port

--- main.go
+++ main.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+	fmt.Print("Now browse to localhost:" + port)
 	http.HandleFunc("/", HelloServer)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":" + port, nil)
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
