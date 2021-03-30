fa2c14dfe520acf42717766acb97df1ae430313d
Kaigo <10222150+hex6b@users.noreply.github.com>
2021-03-30 23:37:09 +0100

    Update HomeHandler.go

--- controller/HomeHandler.go
+++ controller/HomeHandler.go
@@ -7,7 +7,6 @@ import (
 )
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	//w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(model.Response{Status: "UP"})
 }
